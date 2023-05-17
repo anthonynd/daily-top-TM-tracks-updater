5056b3d0b3e78925d182a384102c27c0d671400a
root <root@DESKTOP-54T6790.localdomain>
2023-05-16 18:53:14 -0700

    remove prints

--- update.sh
+++ update.sh
@@ -6,13 +6,10 @@ map_response=$(curl -sS "https://trackmania.exchange/mapsearch2/search?api=on&mo
 if [[ $? -eq 0 ]]; then
     map_IDs=$(echo "$map_response" | jq -r '.results[]' | jq -r '.TrackUID')
 else
-    printf "Request failed"
+    printf "Request failed\n"
 fi
 
 maps_string=$(echo "$map_IDs" | sed 's/.*/"&"/' | tr '\n' ',')
-printf "Maps: $maps_string"
-
-printf "\n"
 
 printf " > Authenticating to Trackmania API...\n"
 
@@ -20,24 +17,18 @@ printf " > Authenticating to Trackmania API...\n"
 ticket_response=$(curl -sS -X POST -u "$TRACKMANIA_API_USERNAME:$TRACKMANIA_API_PASSWORD" -H "Content-Type: application/json" -H "Ubi-AppId: 86263886-327a-4328-ac69-527f0d20a237" -A "Daily top TM trackers updater / https://github.com/anthonynd/daily-top-TM-tracks-updater" -d '{"audience":"NadeoLiveServices"}' "https://public-ubiservices.ubi.com/v3/profiles/sessions")
 if [[ $? -eq 0 ]]; then
     ticket=$(echo "$ticket_response" | jq -r '.ticket')
-    printf "Ticket: $ticket\n"
 else
     # Request failed
-    printf "Request failed"
+    printf "Request failed\n"
 fi
-
-printf "\n"
 
 token_response=$(curl -sS -X POST -H "Content-Type: application/json" -H "Authorization: ubi_v1 t=$ticket" -d '{"audience":"NadeoLiveServices"}' "https://prod.trackmania.core.nadeo.online/v2/authentication/token/ubiservices")
 if [[ $? -eq 0 ]]; then
     token=$(echo "$token_response" | jq -r '.accessToken')
-    printf "Token: $token\n"
 else
     # Request failed
-    printf "Request failed"
+    printf "Request failed\n"
 fi
-
-printf "\n"
 
 data='{
     "name":"MONTHLY TOP TRACKS",
@@ -54,17 +45,12 @@ data='{
     "password":0
 }'
 
-echo $data
-
 printf " > Updating tracks in room...\n"
 
 club_response=$(curl -sS -X POST -H "Authorization: nadeo_v1 t=$token" -d "$data" "https://live-services.trackmania.nadeo.live/api/token/club/61598/room/387678/edit")
-printf "$club_response"
 if [[ $? -eq 0 ]]; then
-    printf "$club_response"
+    printf "Club response: $club_response\n"
 else
-    printf "Request failed"
+    printf "Request failed\n"
 fi
-
-printf "\n"
 
